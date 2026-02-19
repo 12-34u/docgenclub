56b840d91794ff4c36be514ed62470caa36b236b
craniacshencil <rajnikamrajnikam@gmail.com>
2026-02-19 18:29:08 +0530

    Add speakers form field

--- my-app/src/formStore.js
+++ my-app/src/formStore.js
@@ -1,28 +1,29 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 const initialFormData = {
-    eventName: "",
-    eventDate: "",
-    eventVenue: "",
-    eventStartTime: "",
-    eventEndTime: "",
-    eventFlow: "",
-    docType: "Instagram",
-};
+  eventName: '',
+  eventDate: '',
+  eventVenue: '',
+  eventStartTime: '',
+  eventEndTime: '',
+  eventFlow: '',
+  docType: 'Instagram',
+  speakers: [{ name: '', designation: '' }],
+}
 
 export const useFormStore = create(
-    persist(
-        (set) => ({
-            formData: initialFormData,
-            setFormData: (updates) =>
-                set((state) => ({
-                    formData: { ...state.formData, ...updates },
-                })),
-            resetFormData: () => set({ formData: initialFormData }),
-        }),
-        {
-            name: "equinox-form-storage", // key in localStorage
-        },
-    ),
-);
+  persist(
+    (set) => ({
+      formData: initialFormData,
+      setFormData: (updates) =>
+        set((state) => ({
+          formData: { ...state.formData, ...updates },
+        })),
+      resetFormData: () => set({ formData: initialFormData }),
+    }),
+    {
+      name: 'equinox-form-storage', // key in localStorage
+    }
+  )
+)
